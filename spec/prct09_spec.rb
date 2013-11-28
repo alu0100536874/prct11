39becf9e6f0bcc9d46217b0d2a020caf7a42afc4
Krishna Hemnani <alu0100536874@ull.edu.es>
2013-11-28 12:53:09 +0000

    Añadiendo pruebas rspec

--- spec/prct09_spec.rb
+++ spec/prct09_spec.rb
@@ -1,25 +1,74 @@
-require './lib/prct09.rb'
+require "./lib/matriz.rb"
+#require "matriz.rb"        
 
-describe Matrix do
-        before :each do
-		@ma = MatrixDens.new(2,2)
-		@ma.rellenar([[Fraccion.new(1,2),2],[1,Fraccion.new(1,2)]])
-	
-		@mb = MatrixDens.new(2,2)
-		@mb.rellenar([[1,1],[1,1]])
-		
-        end
-	
-        describe "Suma Matriz " do
-                it "Suma" do
-			  
-                        (@ma + @mb).mostrar.should eq("   1 1 \n   1 1 \n ")
+        describe Matrix do
+                before :each do
+		   @ma = MatrixDens.new(2,2)
+		   @ma.rellenar([[Fraccion.new(1,2),2],[1,Fraccion.new(1,2)]])
+		   @mb = MatrixDens.new(2,2)
+		   @mb.rellenar([[1,1],[1,1]])
+		   @mr = MatrixDens.new(2,2)
+		   @mc = SparseMatrix.new(2,2)
+		   @mc.rellenar([[1,0],[0,0]])
+
                 end
-        end
-	
-	describe "Resta Matriz" do
-                it "Resta" do
-                        (@ma - @mb).mostrar.should eq("   -2/4 0 \n   2/6 -6/10 \n ")
+               
+                describe "Comprobación de:" do
+                  it "filas de la matriz" do
+                     @ma.m.should eq(2)
+                  end
+                  it "columnas de la matriz" do
+                     @ma.n.should eq(2)
+                  end
                 end
-	end
-end
+                                    
+                
+                describe "Mostración de Matrices: " do
+                
+                 it " Matriz densa con racionales" do
+                        @ma.mostrar.should eq("  1/2 2 \n  1 1/2 \n ")
+                 end
+		  
+                 it " Matriz densa normal" do
+                        @mb.mostrar.should eq("  1 1 \n  1 1 \n ")
+                 end
+		  
+                 it " Matriz dispersa" do
+                        @mc.mostrarD.should eq ([[1, 0], [0, 0]])
+		 end
+                
+                end
+                
+                describe "Operaciones con matrices densas: " do
+                
+                 it " Suma de matrices" do
+		  @mr = @ma+@mb
+		  @mr.mostrar.should eq("  3/2 3 \n  2 3/2 \n ")
+                 end
+		 
+		 it " Resta de matrices" do
+		  @mr = @ma-@mb
+		  @mr.mostrar.should eq("  -1/2 1 \n  0 -1/2 \n ")
+                 end     
+		 
+		end
+                
+		 describe "Operaciones con matrices densas y dispersas: " do
+                
+                 it " Suma de matrices" do
+		    @mr = @mb+@mc
+		    @mr.mostrar.should eq("  2 1 \n  1 1 \n ")
+                 end
+		 
+		  it " Resta de matrices 1 (Densa - Dispersa)" do
+		    @mr = @mb-@mc
+		    @mr.mostrar.should eq("  0 1 \n  1 1 \n ")
+                 end     
+		 
+		  it " Resta de matrices 2 (Dispersa - Densa)" do
+		    @mr = @mc-@mb
+		    @mr.mostrar.should eq("  0 -1 \n  -1 -1 \n ")
+		  end    
+		end
+                  
+       end
